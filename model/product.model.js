6fb632bb7484b12234a888130ec12010a8e8151c
yasg-uru <yashpawar12122004@gmail.com>
2024-09-30 23:28:05 +0530

    created product model

---
+++ model/product.model.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  productId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  productImage: {
+    type: String,
+    required: [true, "Product Image is required"],
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: mongoose.Decimal128,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+});
+
+productSchema.path("rating").validate((value) => {
+  return value >= 0 && value <= 5;
+}, "Rating must be between 0 and 5.");
+
+const ProductModel = mongoose.model("Product", productSchema);
+
+export default ProductModel;
