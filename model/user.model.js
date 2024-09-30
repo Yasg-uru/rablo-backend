82e3415b595445df770114bc35459f356e91d2d2
yasg-uru <yashpawar12122004@gmail.com>
2024-09-30 21:56:37 +0530

    created user model

---
+++ model/user.model.js
@@ -0,0 +1,38 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Username is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: [true, "Email should be unique"],
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        "Please enter a valid email",
+      ], // Regex to validate email format
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password should be at least 6 characters long"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now, // Automatically sets the creation date
+    },
+    role: {
+      type: String,
+      enum: ["customer", "admin"],
+      default: "customer",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("User", userSchema);
--- utils/connectDb.js
+++ utils/connectDb.js
@@ -1,11 +1,11 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-export const connectDb=async ()=>{
-    try {
-        const response=await mongoose.connect(process.env.MONGO_URI);
-        console.log(`database is connected with : ${response.connection.host}`)
-    } catch (error) {
-        console.log(`error in connectivity of database `);
-       process.exit(1); 
-    }
-}
+export const connectDb = async () => {
+  try {
+    const response = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`database is connected with : ${response.connection.host}`);
+  } catch (error) {
+    console.log(`error in connectivity of database `);
+    process.exit(1);
+  }
+};
