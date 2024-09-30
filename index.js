5fdb7509b22b3645bdba61afe68b0cfcc2d0ca69
yasg-uru <yashpawar12122004@gmail.com>
2024-09-30 21:51:58 +0530

    configured database connectivity

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { config } from "dotenv";
+import { connectDb } from "./utils/connectDb.js";
 config();
 
-const app=express();
-const PORT=process.env.PORT || 4000
-app.listen(PORT,()=>{
-    console.log(`server is running on port : ${PORT}`)
-})
+const app = express();
+const PORT = process.env.PORT || 4000;
+connectDb();
+app.listen(PORT, () => {
+  console.log(`server is running on port : ${PORT}`);
+});
